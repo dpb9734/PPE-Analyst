55eb83f3efae2df7226329b0f37a7823901a45bc
dpb9734 <dpb9734@nyu.edu>
2026-04-12 15:16:24 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import React from "react";
-
 export const metadata = {
   title: "PPE Dashboard",
   description: "PPE Sensitivity Analysis Tool",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
