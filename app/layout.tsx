44159c73a8d9fedf3b6f30ea2971d21f847fa990
dpb9734 <dpb9734@nyu.edu>
2026-04-12 14:54:06 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "PPE Dashboard",
+  description: "PPE Sensitivity Analysis Tool"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
